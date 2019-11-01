6252675252c93b44f866378fbe070b0ce8bdcb10
yoshwata <yoshwata@yahoo-corp.jp>
2019-11-01 13:41:59 +0900

    remove \n

--- .gitconfig.sh
+++ .gitconfig.sh
@@ -4,7 +4,7 @@ git config --global alias.br "branch"
 git config --global alias.st "status"
 git config --global alias.ls "log --pretty=format:\"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]\" --decorate"
 git config --global alias.ll "log --pretty=format:\"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]\" --decorate --numstat"
-git config --global alias.tr "log --all --graph --decorate=short --color --format=format:'%C(bold blue)%h%C(reset) %C(auto)%d%C(reset)\n         %C(blink yellow)[%cr]%C(reset)  %x09%C(white)%an: %s %C(reset)'"
+git config --global alias.tr "log --all --graph --decorate=short --color --format=format:'%C(bold blue)%h%C(reset) %C(auto)%d%C(reset)         %C(blink yellow)[%cr]%C(reset)  %x09%C(white)%an: %s %C(reset)'"
 
 # I can't mount like these format because the device becomes busy
 # [alias]
